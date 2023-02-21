ae507d39c3d7d0fb593f18f48dd84afb6235ee18
Beaux <beauxbarker@yahoo.com>
2023-02-20 20:34:27 -0800

    updating copy function

--- index.js
+++ index.js
@@ -34,14 +34,17 @@ function mySlider(val) {
 
 }
 
-copyBtn.addEventListener('click', function(){
-    var copyText = document.getElementById('pass-output');
-    // Select the text field
-    copyText.select();
-    copyText.setSelectionRange(0, 99999); // For mobile devices
-    // Copy the text inside the text field
-    navigator.clipboard.writeText(copyText.value);
-})
+
+let text = document.getElementById('pass-output').innerHTML;
+    const copyContent = async () => {
+        try {
+            await navigator.clipboard.writeText(text);
+            console.log('Content copied to clipboard');
+          } catch (err) {
+            console.error('Failed to copy: ', err);
+          }
+        }
+
 
 
 
